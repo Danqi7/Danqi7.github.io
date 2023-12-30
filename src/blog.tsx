d044a6d073a2e645a95bceff843da003e9dbe975
Danqi Liao <danqiliao@gmail.com>
2023-12-30 12:19:41 -0500

    blog post

--- src/blog.tsx
+++ src/blog.tsx
@@ -14,16 +14,6 @@ const posts = [
       datetime: '2023-11-16',
       category: { title: 'Poetry', href: '#' },
     },
-    {
-        id: 2,
-        title: 'Make blockchain uncool again',
-        href: '#',
-        description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        category: { title: 'Distributed Systems', href: '#' },
-      },
     // More posts...
   ]
   
